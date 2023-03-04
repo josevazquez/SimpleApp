84eeed4a662633ecae4d8a22dd813ac54e38a068
Jose Vazquez <jose@teguz.com>
2023-03-04 16:15:12 -0600

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Jose Vazquez on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
