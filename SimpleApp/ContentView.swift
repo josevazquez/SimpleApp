e8c1e8bbec4c2a096024154f20667cec13ee1bb8
Jose Vazquez <jose@teguz.com>
2023-03-04 20:28:39 -0600

    Added some content

--- SimpleApp/ContentView.swift
+++ SimpleApp/ContentView.swift
@@ -7,13 +7,63 @@
 
 import SwiftUI
 
+class MyObject: ObservableObject, Identifiable {
+    var id = UUID()
+    @Published var name: String
+    @Published var value: Int
+    
+    init(name: String, value: Int) {
+        self.name = name
+        self.value = value
+    }
+}
+
+struct MyStruct: Identifiable {
+    var id = UUID()
+    var name: String
+    var value: Int
+}
+
+class ViewModel: ObservableObject {
+    @Published var objects: [MyObject]
+    @Published var structs: [MyStruct]
+    
+    init(objects: [MyObject], structs: [MyStruct]) {
+        self.objects = objects
+        self.structs = structs
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel(
+        objects: [
+            MyObject(name: "Michael", value: 23),
+            MyObject(name: "Leo", value: 10)],
+        structs: [
+            MyStruct(name: "Cristiano", value: 7),
+            MyStruct(name: "Mario", value: 66)
+        ])
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            Section("Objects") {
+                ForEach(viewModel.objects) { o in
+                    HStack {
+                        Text("\(o.name)")
+                        Spacer()
+                        Text("\(o.value)")
+                    }
+                }
+            }
+            Section("Structs") {
+                ForEach(viewModel.structs) { s in
+                    HStack {
+                        Text("\(s.name)")
+                        Spacer()
+                        Text("\(s.value)")
+                    }
+                }
+            }
         }
         .padding()
     }
